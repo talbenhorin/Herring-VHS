910aa38f425a5505568ed20546d8ccb1819bd213
talbenhorin <36824919+talbenhorin@users.noreply.github.com>
2019-07-10 22:37:56 -0400

    corrected parameter errors

--- solutionSEICprevalence.R
+++ solutionSEICprevalence.R
@@ -1,13 +1,13 @@
 rm(list=ls(all=TRUE)) #clears workspace
 
-K = 500000;
-gamma = 0.25;#E -> I
-rho = 0.125;#I -> C
-lambda = 0.4;#force of infection
-b = 1.2;#births
-mu = 0.15;#natural mortality
-c <- (b - mu)/K;#strength of density dependence
-alpha = 1;#disease mortality
+K = 500000
+gamma = 91.25#E -> I
+rho = 30#I -> C
+lambda = 0.5#force of infection
+b = 1.2#births
+mu = 0.15#natural mortality
+c <- (b - mu)/K#strength of density dependence
+alpha = 2#disease mortality
 
 f <- seq(0,0.15,length=50)
 
