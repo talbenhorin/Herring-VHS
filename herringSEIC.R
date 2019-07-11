af70bd2834cacbb73da9c0f5283307669a95bdec
talbenhorin <36824919+talbenhorin@users.noreply.github.com>
2019-07-10 23:21:20 -0400

    added frequency dependent transmission

--- herringSEIC.R
+++ herringSEIC.R
@@ -12,8 +12,8 @@ seic <- function(time, state, parameters) {
   
   with(as.list(c(state, parameters)), {
   
-    dS <- (b-c*(S+E+I+C))*(S+E+I+C) - (mu+f+lambda)*S 
-    dE <- lambda*S - (mu+f+gamma)*E
+    dS <- (b-c*(S+E+I+C))*(S+E+I+C) - (mu+f)*S - beta*S*(I/(S+E+I+C)) 
+    dE <- beta*S*(I/(S+E+I+C)) - (mu+f+gamma)*E
     dI <- gamma*E - (mu+f+alpha+rho)*I
     dC <- rho*I - (mu+f)*C
     
@@ -23,7 +23,7 @@ seic <- function(time, state, parameters) {
 
 ### Set parameters
 init       <- c(S= 1000000, E = 100, I = 0, C = 0)
-parameters <- c(gamma = 91.25, rho = 30, lambda = 0.5, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 2,f = 0.15)
+parameters <- c(gamma = 91.25, rho = 30, beta = 45, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 2,f = 0)
 
 ## Time frame
 times      <- seq(0, 50, by = 0.5)
@@ -32,9 +32,6 @@ times      <- seq(0, 50, by = 0.5)
 out <- ode(y = init, times = times, func = seic, parms = parameters)
 ## change to data frame
 out <- as.data.frame(out)
-
-## Show data
-head(out, 10)
 
 ## Plot
 plot(times,out$S+out$E+out$I+out$C)
