5f4a3c0836b4a5acbf2f90d7208a65ba89e0174d
talbenhorin <36824919+talbenhorin@users.noreply.github.com>
2019-07-12 10:34:09 -0400

    clean up

--- herringSEIC.R
+++ herringSEIC.R
@@ -27,9 +27,10 @@ fishing <- seq(0,1,length=10)
 times      <- seq(0, 50, by = 0.5)
 
 eqsize <- numeric(length(fishing)) # a vector to hold the solutions
+eqprev <- numeric(length(fishing))
 
 for(i in seq_along(fishing)){
-  parameters <- c(qS = 0.5,qC = 1,gamma = 30, rho = 91.25, beta = 45, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 2,f = fishing[i])
+  parameters <- c(qS = 1,qC = 1,gamma = 30, rho = 91.25, beta = 45, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 2,f = fishing[i])
   out <- as.data.frame(
     out <- ode(
       y = init, 
