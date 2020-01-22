696818ef50faebc50e2a7a1aa8a8d75b27155ce7
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2020-01-22 10:44:23 -0500

    Cut out the loop, integrate the SEIC model for a single set of parameter values

--- herringSEIC.R
+++ herringSEIC.R
@@ -1,8 +1,8 @@
 rm(list=ls(all=TRUE)) #clears workspace
 
 # Install these packages if you haven't already
-# install.packages("Rtools")
-# install.packages("deSolve")
+install.packages("Rtools")
+install.packages("deSolve")
 
 # Load deSolve package
 library(deSolve)
@@ -26,31 +26,15 @@ init       <- c(S= 1000000, E = 100, I = 0, C = 0)
 fishing <- seq(0,1,length=10)
 times      <- seq(0, 50, by = 0.5)
 
-eqsize.cold <- numeric(length(fishing)) # a vector to hold the solutions
-eqsize.warm <- numeric(length(fishing)) # a vector to hold the solutions
+#eqsize.cold <- numeric(length(fishing)) # a vector to hold the solutions
+#eqsize.warm <- numeric(length(fishing)) # a vector to hold the solutions
 
-for(i in seq_along(fishing)){
-  p.cold <- c(qS = 0.1,qC = 1,gamma = 91.25, rho = 30, beta = 45, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 2,f = fishing[i])
-  p.warm <- c(qS = 0.1,qC = 1,gamma = 91.25, rho = 91.25, beta = 45, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 0.1,f = fishing[i])
-  out.cold <- as.data.frame(
-    out.cold <- ode(
-      y = init, 
-      times = times, 
-      func = seic, 
-      parms = p.cold
-      )
-    )
-  out.warm <- as.data.frame(
-    out.warm <- ode(
-      y = init, 
-      times = times, 
-      func = seic, 
-      parms = p.warm
-    )
+p <- c(qS = 0.1,qC = 1,gamma = 91.25, rho = 30, beta = 45, b = 0.6, mu = 0.15, c = 1.05e-06,alpha = 2,f = 0)#parameter string
+out <- ode(
+  y = init,
+  times = times,
+  func = seic,
+  parms = p
   )
-  eqsize.warm[i] <- tail(out.warm$S+out.warm$E+out.warm$I+out.warm$C,1)
-  eqsize.cold[i] <- tail(out.cold$S+out.cold$E+out.cold$I+out.cold$C,1)
-}
 
-plot(fishing,eqsize.warm,type='l',col='red')
-points(fishing,eqsize.cold,type='l',col='blue')
+plot(out,type='l',col='blue')
